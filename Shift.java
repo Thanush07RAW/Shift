74588d8e76b6708422acfe09ebca5029156d369f
Thanush07RAW <draken0067@gmail.com>
2025-01-29 21:59:28 +0530

    Add files via upload

---
+++ Shift.java
@@ -0,0 +1,12 @@
+public class Shift {
+    public static void main(String[] args) {
+        int a = 8;
+        int result = a << 9;
+        System.out.printf("The left shift of %d is: %d", a, result);
+
+        int b = 17;
+        int res = b >> 9; 
+        System.out.printf("\nThe right shift of %d is : %d", b, res); 
+    }
+}
+
